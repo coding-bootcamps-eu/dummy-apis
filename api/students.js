f0fc1d0f253e77fdf1a8a6b37fd6f2467b198372
Nico König <npunktkoenig@gmail.com>
2022-10-11 14:09:11 +0200

    feat: adds students api

---
+++ api/students.js
@@ -0,0 +1,18 @@
+exports.handler = async function (event, context, callback) {
+  const { students } = require(`./students.json`);
+
+  try {
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET",
+      },
+      body: JSON.stringify(students),
+    });
+  } catch (error) {
+    callback(error);
+  }
+};
